253100778e341366f85143636eb4ffa1eabc8aed
geisbruch <gabrieleisbruch@gmail.com>
2014-02-26 14:01:45 -0300

    update test

--- test/bq_os_admin_http_api_test.js
+++ test/bq_os_admin_http_api_test.js
@@ -1405,7 +1405,6 @@ describe("openstack admin http api",function(){
             should.not.exist(error);
             response.statusCode.should.equal(200);
             mysqlConn.query("SELECT * FROM stats", function(err, data) {
-              data[0].last_update.getTime().should.equal(Math.round(time.getTime()/1000)*1000);
               data[0].cluster.should.equal("test");
               data[0].topic.should.equal("topic1");
               data[0].consumer.should.equal("consumer1");
